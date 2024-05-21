cdcbe94bb4a444811d7370f7bcefcc852e478e19
Sarah McLoughlin <boosarahs@gmail.com>
2024-05-21 14:42:40 +0000

    Add info modal with content

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,30 @@ function togglePlay() {
         }
     }
 }
+
+// Modal - adjusted to use click on image (not button) to open modal https://www.w3schools.com/howto/howto_css_modals.asp
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image that opens the modal
+var img = document.getElementById("info-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the image, open the modal
+img.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
