9e94d5e97d09eab9a14a8375e56ba3fa7cce6b58
Sarah McLoughlin <boosarahs@gmail.com>
2024-05-21 19:00:38 +0000

    Import slidles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 /** Audio */
 // Toggle On Off https://stackoverflow.com/questions/55018585/how-to-turn-on-audio-on-click-icon-play-pause
 function togglePlay() {
-    let audio = document.getElementsByTagName("audio")[0];
-    if (audio) {
-        if (audio.paused) {
-            audio.play();
-            document.getElementById("volume-icon").src = "assets/images/vol-on.webp";
-        } else {
-            audio.pause();
-            document.getElementById("volume-icon").src = "assets/images/vol-off.webp";
-        }
-    }
+  let audio = document.getElementsByTagName("audio")[0];
+  if (audio) {
+      if (audio.paused) {
+          audio.play();
+          document.getElementById("volume-icon").src = "assets/images/vol-on.webp";
+      } else {
+          audio.pause();
+          document.getElementById("volume-icon").src = "assets/images/vol-off.webp";
+      }
+  }
 }
 
 // Modal - source tutorial: https://www.w3schools.com/howto/howto_css_modals.asp adjusted to use click on image (not button) to open modal
@@ -25,17 +25,17 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the image, open the modal
 img.onclick = function() {
-  modal.style.display = "block";
+modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+if (event.target == modal) {
+  modal.style.display = "none";
+}
 }
